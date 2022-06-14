745dca83ea5f4c59a92239ef6c7f5998e528a51d
LeonidGHA <stowk1992@gmail.com>
2022-06-14 20:51:46 +0300

    add reviewer section all

---
+++ src/js/read-more.js
@@ -0,0 +1,97 @@
+$('#showr1').click(function () {
+  $('.reviews__fake-text').show('slow');
+  $('.reviews__dots').hide('slow');
+  $('.reviews__fake-hide').show('slow');
+  $('.reviews__fake-read').hide('slow');
+});
+
+$('#hidr1').click(function () {
+  $('.reviews__fake-text').hide('fast');
+  $('.reviews__dots').show('slow');
+  $('.reviews__fake-read').show('slow');
+  $('.reviews__fake-hide').hide('slow');
+});
+
+$('#showr2').click(function () {
+  $('.reviews__fake-text').show('slow');
+  $('.reviews__dots').hide('slow');
+  $('.reviews__fake-hide').show('slow');
+  $('.reviews__fake-read').hide('slow');
+});
+
+$('#hidr2').click(function () {
+  $('.reviews__fake-text').hide('fast');
+  $('.reviews__dots').show('slow');
+  $('.reviews__fake-read').show('slow');
+  $('.reviews__fake-hide').hide('slow');
+});
+
+$('#showr3').click(function () {
+  $('.reviews__fake-text').show('slow');
+  $('.reviews__dots').hide('slow');
+  $('.reviews__fake-hide').show('slow');
+  $('.reviews__fake-read').hide('slow');
+});
+
+$('#hidr3').click(function () {
+  $('.reviews__fake-text').hide('fast');
+  $('.reviews__dots').show('slow');
+  $('.reviews__fake-read').show('slow');
+  $('.reviews__fake-hide').hide('slow');
+});
+
+$('#showr4').click(function () {
+  $('.reviews__fake-text').show('slow');
+  $('.reviews__dots').hide('slow');
+  $('.reviews__fake-hide').show('slow');
+  $('.reviews__fake-read').hide('slow');
+});
+
+$('#hidr4').click(function () {
+  $('.reviews__fake-text').hide('fast');
+  $('.reviews__dots').show('slow');
+  $('.reviews__fake-read').show('slow');
+  $('.reviews__fake-hide').hide('slow');
+});
+
+$('#showr5').click(function () {
+  $('.reviews__fake-text').show('slow');
+  $('.reviews__dots').hide('slow');
+  $('.reviews__fake-hide').show('slow');
+  $('.reviews__fake-read').hide('slow');
+});
+
+$('#hidr5').click(function () {
+  $('.reviews__fake-text').hide('fast');
+  $('.reviews__dots').show('slow');
+  $('.reviews__fake-read').show('slow');
+  $('.reviews__fake-hide').hide('slow');
+});
+
+$('#showr6').click(function () {
+  $('.reviews__fake-text').show('slow');
+  $('.reviews__dots').hide('slow');
+  $('.reviews__fake-hide').show('slow');
+  $('.reviews__fake-read').hide('slow');
+});
+
+$('#hidr6').click(function () {
+  $('.reviews__fake-text').hide('fast');
+  $('.reviews__dots').show('slow');
+  $('.reviews__fake-read').show('slow');
+  $('.reviews__fake-hide').hide('slow');
+});
+
+$('#showr7').click(function () {
+  $('.reviews__fake-text').show('slow');
+  $('.reviews__dots').hide('slow');
+  $('.reviews__fake-hide').show('slow');
+  $('.reviews__fake-read').hide('slow');
+});
+
+$('#hidr7').click(function () {
+  $('.reviews__fake-text').hide('fast');
+  $('.reviews__dots').show('slow');
+  $('.reviews__fake-read').show('slow');
+  $('.reviews__fake-hide').hide('slow');
+});
--- src/js/slider.js
+++ src/js/slider.js
@@ -4,21 +4,36 @@ $('.slider-for').slick({
   arrows: false,
   infinite: false,
   asNavFor: '.slider-nav',
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        prevArrow: $('[data-slick-left]'),
+        nextArrow: $('[data-slick-right]'),
+        arrows: true,
+      },
+    },
+  ],
 });
 $('.slider-nav').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   asNavFor: '.slider-for',
   infinite: false,
-  arrows: false,
+  arrows: true,
+  adaptiveHeight: true,
   dots: false,
   focusOnSelect: true,
   mobileFirst: true,
+  prevArrow: $('.reviews__button-left'),
+  nextArrow: $('.reviews__button-right'),
   responsive: [
     {
       breakpoint: 767,
       settings: {
         slidesToShow: 7,
+        arrows: false,
       },
     },
   ],
