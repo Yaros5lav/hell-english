0c7aedce9e18347b174fc0b6954dc7962ee8b482
LeonidGHA <stowk1992@gmail.com>
2022-06-16 15:59:48 +0300

    add button fix last

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,39 +1,29 @@
 $('.slider-for').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: false,
+  dots: false,
   infinite: false,
   asNavFor: '.slider-nav',
   mobileFirst: true,
   prevArrow: $('[data-slick-left]'),
   nextArrow: $('[data-slick-right]'),
-  responsive: [
-    {
-      breakpoint: 767,
-      settings: {
-        arrows: true,
-      },
-    },
-  ],
 });
+
 $('.slider-nav').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   asNavFor: '.slider-for',
   infinite: false,
-  arrows: true,
+  arrows: false,
   dots: false,
   focusOnSelect: true,
   mobileFirst: true,
-  prevArrow: $('.reviews__button-left'),
-  nextArrow: $('.reviews__button-right'),
   responsive: [
     {
       breakpoint: 767,
       settings: {
         slidesToShow: 7,
         slidesToScroll: 7,
-        arrows: false,
       },
     },
   ],
