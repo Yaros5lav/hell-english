00efd0e8d063299375db56f3ab994d2dc9861239
Valeriia Siabruk <vsyabryk@gmail.com>
2022-06-16 11:51:27 +0300

    add script in mobile menu and fix menu position

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,24 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+  const body = document.querySelector('body');
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const linkHeaderMenuList = document.querySelectorAll('.navigation__link');
 
-  menuBtnRef.addEventListener("click", () => {
+  menuBtnRef.addEventListener('click', () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnRef.classList.toggle('is-open');
+    body.classList.toggle('overflow-hidden');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    mobileMenuRef.classList.toggle('is-open');
+  });
 
-    mobileMenuRef.classList.toggle("is-open");
+  linkHeaderMenuList.forEach(link => {
+    link.addEventListener('click', () => {
+      mobileMenuRef.classList.toggle('is-open');
+      body.classList.toggle('overflow-hidden');
+      menuBtnRef.classList.toggle('is-open');
+    });
   });
 })();
--- src/js/modal.js
+++ src/js/modal.js
@@ -13,3 +13,10 @@ const toggleModal = function () {
 closeButton.addEventListener('click', toggleModal);
 modalOverlay.addEventListener('click', toggleModal);
 openButton.addEventListener('click', toggleModal);
+
+// const LinkHeaderMenu = document.querySelectorAll('.navigation__link');
+// LinkHeaderMenu.addEventListener('click', toggleModal);
+
+// const toggleBurger = function () {
+//   LinkHeaderMenu.classList.toggle('')
+// }
